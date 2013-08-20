a14cf63e9e9e53d4e271a1e2e3268f94628892a3
Hiroaki Nakamura <hnakamur@gmail.com>
2013-08-20 13:20:06 +0900

    Add a generated app by rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneRailsUploadExample::Application.config.secret_key_base = '77c52bfc633edc64e174ad1f49209dd520ebf0716c6343fa5106dbf55a3c0994a38f0487b28c9efb70f45f6ac75405718e27f380f98c73f92dcd6ef90a275a1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneRailsUploadExample::Application.config.session_store :cookie_store, key: '_backbone_rails_upload_example_session'
